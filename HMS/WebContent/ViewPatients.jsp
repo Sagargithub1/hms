1fcd93dc17bc395214e357c11daface312438b90
sagar <sagar@sagar-desktop>
2020-06-29 21:37:16 +0530

    View patient implemented

---
+++ HMS/WebContent/ViewPatients.jsp
@@ -0,0 +1,43 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@page import="com.hms.dao.*,com.hms.bean.*,java.util.*" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>View Patients</title>
+</head>
+<body>
+<h2>View Patients</h2>
+
+<%
+	List<PatientBean> list=PatientDao.getAllRecords();
+	request.setAttribute("list", list);
+	
+%>
+
+  
+<table border="1" >
+<tr><th>Patient Id</th><th>Name</th><th>Age</th><th>Address</th>  
+<th>DOJ</th><th>Type Of Room</th><th>Edit</th><th>Delete</th></tr> 
+
+<c:forEach items="${list}" var="u">
+
+<tr><td>${u.getPatientId()}</td><td>${u.getPatientName()}</td><td>${u.getPatientAge()}</td>  
+<td>${u.getPatientAddress()}</td><td>${u.getDateOfJoining()}</td><td>${u.getTypeOfBed()}</td>  
+<td><a href="#">Edit</a></td>  
+<td><a href="#">Delete</a></td></tr>  
+</c:forEach>
+<!-- 
+<td><a href="editform.jsp?id=${u.getId()}">Edit</a></td>  
+<td><a href="deleteuser.jsp?id=${u.getId()}">Delete</a></td></tr>  
+ -->
+
+</table>
+
+
+
+
+</body>
+</html>
--- HMS/src/com/hms/dao/PatientDao.java
+++ HMS/src/com/hms/dao/PatientDao.java
@@ -2,13 +2,18 @@ package com.hms.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 import com.hms.bean.PatientBean;
 import com.hms.utils.DBConnection;
 
 public class PatientDao {
-
+	
+	public static String FETCH_ALL_RECORDS_SQL="SELECT * FROM pat_test WHERE patient_status='Active';";
+	
 	public static int addPatient(PatientBean patient) {
 		int result = 0;
 		//create a java timestamp object that represents the current time (i.e., a "current timestamp")
@@ -42,4 +47,61 @@ public class PatientDao {
 
 		return result;
 	}
+	
+	public static List<PatientBean> getAllRecords(){
+		List<PatientBean> list= new ArrayList<PatientBean>();
+		
+		try {
+			Connection con=DBConnection.createConnection();
+			PreparedStatement ps=con.prepareStatement(FETCH_ALL_RECORDS_SQL);
+			ResultSet rs=ps.executeQuery();
+			while(rs.next()) {
+				PatientBean pat=new PatientBean();
+				
+				pat.setPatientId(rs.getInt("patient_id"));
+				pat.setPatientSsnNo(rs.getInt("patient_ssn"));
+				pat.setPatientName(rs.getString("patient_name"));
+				pat.setPatientAddress(rs.getString("patient_address"));
+				pat.setPatientAge(rs.getInt("patient_age"));
+				pat.setPatientState(rs.getString("patient_state"));
+				pat.setPatientCity(rs.getString("patient_city"));
+				pat.setDateOfJoining(rs.getTimestamp("patient_doj"));
+				pat.setStatus(rs.getString("patient_status"));
+				pat.setTypeOfBed(rs.getString("patient_typeOfBed"));
+				list.add(pat);
+				System.out.println("Record taken");
+				
+			}
+			
+		}catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e);
+		}
+		
+		return list;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
